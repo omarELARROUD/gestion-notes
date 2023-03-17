23101f7a9d7410b8161c1e4247123b1d87fefef6
sohaib-idalin <sohaibidalin2017@gmail.com>
2023-03-17 16:38:52 +0100

    M1

--- dll/Connection.cs
+++ dll/Connection.cs
@@ -1,6 +1,7 @@
 ﻿using MySql.Data.MySqlClient;
 using System.Data;
 using System.Data.OracleClient;
+using System.Xml;
 
 namespace DB
 {
@@ -10,47 +11,51 @@ namespace DB
         static IDbCommand cmd = null;
 
         
-        public static void Connect(string sgbd="mysql" ,string str_con= "server=localhost;uid=root;pwd='';database=ginf2")
+        public static void Connect()
         {
+            string filePath = "./env.xml";
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(filePath);
 
-            if (sgbd == "mysql")
+            // Get the root element
+            XmlElement root = xmlDoc.DocumentElement;
+
+            // the DB_CONNECTION attribute
+            string dbManager = root.GetAttribute("DB_CONNECTION");
+
+            // the database connection parameters
+            string dbHost = root.GetAttribute("DB_HOST");
+            string dbPort = root.GetAttribute("DB_PORT");
+            string dbName = root.GetAttribute("DB_DATABASE");
+            string dbUsername = root.GetAttribute("DB_USERNAME");
+            string dbPassword = root.GetAttribute("DB_PASSWORD");
+
+            if (dbManager == "mysql")
             {
-                //string str_con = "server=localhost;uid=root;pwd='';database=ginf2";
+                string str_con = $"server={dbHost};uid={dbUsername};pwd={dbPassword};database={dbName}";
                 con = new MySqlConnection(str_con);
                 cmd = con.CreateCommand();
 
-                try
-                {
-                    con.Open();
-                    return;
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
-                }
-
-            }
-            else
+            }else if (dbManager == "oracle")
             {
-                //str_con = "DATA SOURCE=localhost:1521/orcl;PASSWORD=123;USER ID=optim21";
+                string str_con = $"DATA SOURCE={dbHost}:{dbPort}/{dbName};PASSWORD={dbPassword};USER ID={dbUsername}";
                 con = new OracleConnection(str_con);
                 cmd = con.CreateCommand();
+            }
+            try
+            {
+                con.Open();
+                return;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                con = null;
+                cmd = null;
+                throw new Exception("can not connect");
 
-                try
-                {
-                    con.Open();
-                    return;
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
-                }
             }
 
-            con = null;
-            cmd = null;
-            throw new Exception("can not connect");
-            
 
         }
         public static int IUD(string req)
@@ -84,6 +89,35 @@ namespace DB
             }
             
             return champs;
+        }
+
+        public static void execute_procedure(string proc, Dictionary<string,object> dico)
+        {
+            cmd.CommandText = proc;
+            cmd.CommandType = CommandType.StoredProcedure;
+            foreach(var kv in dico)
+            {
+                cmd.Parameters.Add(new MySqlParameter(kv.Key, kv.Value));
+            }
+            cmd.ExecuteNonQuery();
+            cmd=con.CreateCommand();
+        }
+
+        public static dynamic execute_function(string func, Dictionary<string, object> dico)
+        {
+            cmd.CommandText = func;
+            cmd.CommandType = CommandType.StoredProcedure;
+            foreach (var kv in dico)
+            {
+                cmd.Parameters.Add(new MySqlParameter(kv.Key, kv.Value));
+            }
+            MySqlParameter returnParam = new MySqlParameter();
+            returnParam.Direction = ParameterDirection.ReturnValue;
+            cmd.Parameters.Add(returnParam);
+            cmd.ExecuteNonQuery();
+            var res = returnParam.Value;
+            cmd = con.CreateCommand();
+            return res;
         }
 
     }
--- dll/Model.cs
+++ dll/Model.cs
@@ -6,12 +6,15 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace DB
 {
     public abstract class Model
     {
         private int id = 0;
+        private static Boolean log=false;
+        protected static List<string> Tlog=new List<string>();
 
         public int Id { get => id; set => id = value; }
 
@@ -33,24 +36,67 @@ namespace DB
             }
             return obj;
         }
-        public int save()
+        public static int maxid(string table_name, string func_maxid = null)
         {
-            var table_name = this.GetType().Name;
-            if (id == 0)
+            if (func_maxid != null)
             {
+                var dico=new Dictionary<string, object>();
+                dico.Add("table_name", table_name);
+                return Connection.execute_function(func_maxid, dico);
+            }
+            var sql = $"SELECT count(id) FROM {table_name}";
+            var reader = Connection.Select(sql);
+            reader.Read();
+            if (reader.GetInt32(0)==0)
+            {
+                reader.Close();
+                return 0;
+            }
+            reader.Close();
+            sql = $"SELECT max(id) FROM {table_name}";
+            reader = Connection.Select(sql);
+            reader.Read();
+            int res = reader.GetInt32(0);
+            reader.Close();
+            return res;
+        }
+        public int save(string func_save=null,string func_maxeid=null)
+        {
+            var table_name = this.GetType().Name +"s";
+            var oldobj = this.find();
+            var res = 0;
+            if(func_save != null)
+            {
+                var dico = ObjectToDictionary(this);
+                id= Connection.execute_function(func_save, dico);
+                res = 1;
+            }
+            else if (id == 0)
+            {
+                id = maxid(table_name, func_maxeid) +1;
                 var columns = string.Join(",", ObjectToDictionary(this).Keys);
                 //var values = string.Join(",", ObjectToDictionary(this).Values.Select(v => v is string ? "'" + v.ToString() + "'" : v.ToString()));
                 var values = string.Join(",", ObjectToDictionary(this).Values.Select(v => "'" + v.ToString() + "'"));
                 var sql = $"INSERT INTO {table_name} ({columns}) VALUES ({values})";
-                return Connection.IUD(sql);
+                res=Connection.IUD(sql);
             }
             else
             {
                 //var sets = string.Join(",", ObjectToDictionary(this).Select(kvp => kvp.Value is string ? $"{kvp.Key}='{kvp.Value}'" : $"{kvp.Key}={kvp.Value}"));
                 var sets = string.Join(",", ObjectToDictionary(this).Select(kvp => $"{kvp.Key}='{kvp.Value}'"));
                 var sql = $"UPDATE {table_name} SET {sets} WHERE id={id}";
-                return Connection.IUD(sql);
+                res= Connection.IUD(sql);
             }
+
+            if (oldobj == null)
+            {
+                insertxmlline("insert");
+            }
+            else
+            {
+                insertxmlline("update", oldobj);
+            }
+            return res;
 
 
         }
@@ -58,12 +104,17 @@ namespace DB
 
         public dynamic find()
         {
-            var table_name = this.GetType().Name;
+            var table_name = this.GetType().Name+"s";
             var sql = $"SELECT * FROM {table_name} WHERE id={id}";
             var reader = Connection.Select(sql);
-            reader.Read();
+            if (!reader.Read())
+            {
+                reader.Close();
+                return null;
+            }
             var dict = new Dictionary<string, object>();
-            for (var i = 0; i < reader.FieldCount; i++)
+            dict.Add(reader.GetName(0).ToLower(), reader.GetInt32(0));
+            for (var i = 1; i < reader.FieldCount; i++)
             {
                 dict.Add(reader.GetName(i).ToLower(), reader.GetValue(i));
             }
@@ -95,20 +146,24 @@ namespace DB
         }
         public int delete()
         {
-            var table_name = this.GetType().Name;
+            var table_name = this.GetType().Name + "s";
             var sql = $"DELETE FROM {table_name} WHERE id={id}";
-            return Connection.IUD(sql);
+            var obj = find();
+            var res= Connection.IUD(sql);
+            if(obj!=null)obj.insertxmlline("delete");
+            return res;
         }
         public List<dynamic> all()
         {
-            var table_name = this.GetType().Name;
+            var table_name = this.GetType().Name + "s";
             var sql = $"SELECT * FROM {table_name}";
             var reader = Connection.Select(sql);
             var list = new List<dynamic>();
             while (reader.Read())
             {
                 var dict = new Dictionary<string, object>();
-                for (var i = 0; i < reader.FieldCount; i++)
+                dict.Add(reader.GetName(0).ToLower(), reader.GetInt32(0));
+                for (var i = 1; i < reader.FieldCount; i++)
                 {
                     dict.Add(reader.GetName(i).ToLower(), reader.GetValue(i));
                 }
@@ -143,7 +198,7 @@ namespace DB
         }
         public List<dynamic> Select(Dictionary<string, object> dico)
         {
-            var table_name = this.GetType().Name;
+            var table_name = this.GetType().Name + "s";
             var sql = $"SELECT * FROM {table_name} WHERE ";
             sql += string.Join(" AND ", dico.Select(kvp => $"{kvp.Key}='{kvp.Value}'"));
             var reader = Connection.Select(sql);
@@ -151,7 +206,8 @@ namespace DB
             while (reader.Read())
             {
                 var dict = new Dictionary<string, object>();
-                for (var i = 0; i < reader.FieldCount; i++)
+                dict.Add(reader.GetName(0).ToLower(), reader.GetInt32(0));
+                for (var i = 1; i < reader.FieldCount; i++)
                 {
                     dict.Add(reader.GetName(i).ToLower(), reader.GetValue(i));
                 }
@@ -185,6 +241,80 @@ namespace DB
             //}
             //reader.Close();
             //return list;
+        }
+
+        public  static Boolean initializelog()
+        {
+            if(log)return true;
+            try
+            {
+                string path = "./xmllog";
+                Directory.CreateDirectory(path);
+                log = true;
+                return true;
+            }catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return false;
+            }
+            
+        }
+
+        public Boolean activatelog()
+        {
+            if (log)
+            {
+                if (Tlog.Contains(this.GetType().Name.ToLower()))return true;
+                var xml_name = this.GetType().Name.ToLower() + "s";
+                string filePath = "./xmllog/" + xml_name + ".xml";
+                if (!File.Exists(filePath))
+                {
+                    var doc = new XDocument(new XDeclaration("1.0", "utf-8", "no"), new XElement(xml_name));
+                    doc.Save(filePath);
+                }
+                Tlog.Add(this.GetType().Name.ToLower());
+                return true;
+            }
+            return false;
+
+            
+        }
+        public Boolean insertxmlline(string iud,dynamic oldobj=null)
+        {
+            var element_name = this.GetType().Name.ToLower();
+            var xml_name = element_name + "s";
+            var path = "./xmllog/" + xml_name + ".xml";
+            if (log && Tlog.Contains(element_name))
+            {
+                
+                var doc = XDocument.Load(path);
+                var root = doc.Root;
+                var objelm = new XElement(element_name);
+                objelm.SetAttributeValue("opr", iud);
+                objelm.SetAttributeValue("timestamp", DateTime.Now.ToString());
+                var dico = ObjectToDictionary(this);
+                
+                if (iud == "insert" || iud == "delete")
+                {
+                    foreach (var kv in dico)
+                    {
+                        objelm.Add(new XElement(kv.Key, kv.Value));
+                    }
+                }else if(iud=="update" && oldobj != null)
+                {
+                    var olddico= ObjectToDictionary(oldobj);
+                    foreach (var k in dico.Keys)
+                    {
+                        objelm.Add(new XElement(k, new XElement("oldValue", olddico[k]), new XElement("newValue", dico[k])));
+                    }
+                }
+                
+                root.Add(objelm);
+                doc.Save(path);
+                return true;
+
+            }
+            return false;
         }
     }
 }
